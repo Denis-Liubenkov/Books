c294b895129b678940cd6e15c3b175d626892ef9
Alligator89 <denisliubenkov@yandex.by>
2024-10-14 21:17:00 +0300

    create configuration connection db

--- src/main/java/com/tms/config/Config.java
+++ src/main/java/com/tms/config/Config.java
@@ -3,6 +3,10 @@ package com.tms.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.tms")
@@ -16,5 +20,20 @@ public class Config {
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/library");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
