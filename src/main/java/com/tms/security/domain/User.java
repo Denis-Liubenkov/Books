09563eb8fb2fff87ceff0d3a31d9933c0bc05a0f
Alligator89 <denisliubenkov@yandex.by>
2024-12-04 17:25:55 +0300

    add domain User

---
+++ src/main/java/com/tms/security/domain/User.java
@@ -0,0 +1,17 @@
+package com.tms.security.domain;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String username;
+        private String password;
+        @ElementCollection(fetch = FetchType.EAGER)
+        private Set<String> roles = new HashSet<>();
+}
